e54bc3cae22fd720d7435943e884d0987a62bf06
brunoressiguierbarbosa <bruno.ressiguier.barbosa@escola.pr.gov.br>
2024-11-22 08:29:44 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,44 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero
+
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero/numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+//NaN -> Not A Nember(nao e um numero)
+
+const alura = "Alura";
+console.log(alura*primeiroNumero)
+
+//var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura*comprimento;
+//console.log(area)
+//var area;
+
+//let forma = 'retangulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area;
+
+//if(forma === 'retangulo'){
+    //area = altura*comprimento;
+    //}else{
+        //area = (area*comprimento)/2;
+    //}
